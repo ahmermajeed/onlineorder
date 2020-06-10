7a8fb8ea99a4b0f84b11168beb5a464483ad5ab7
owais <owais@bargoventures.com>
2020-06-11 03:48:18 +0500

    total slaes

--- routes/api.php
+++ routes/api.php
@@ -49,6 +49,8 @@ Route::resource('products', 'Api\ProductController');
 /** Place Order */
 Route::post('placeOrder', 'Api\OrderController@placeOrder');
 
+Route::post('get-total-sales', 'Api\OrderController@getTotalSales');
+
 /** CMS Pages */
 Route::get('cms/{slug}', 'Api\CmsController@getCMSPage');
 
