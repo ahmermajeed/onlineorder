530d968236453817030a200b124173e1d6e39987
yousufmunir1 <yousuf@bargoventures.com>
2020-06-12 21:00:30 +0500

    updating

--- resources/js/components/Aboutus.vue
+++ resources/js/components/Aboutus.vue
@@ -33,10 +33,10 @@
                             <div class="col-lg-6 col-md-6 col-sm-12">
                                 <h2>Welcome At <span></span></h2>
                                 <h3>PAPAG RESTAURANT</h3>
-                                <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim anim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                    cillum doore eu fgiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br><br>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                    Ut enim anim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum doore eu fgiat nulla pariatur. Excepteur sint occaecat
-                                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                <p>Papa Ge’s has been established in the uk since 2004 and started off in Tamworth in Staffordshire, it has not reached Birminghak with this new and improved store located at 888 Washwood Heath Road in Birmingham.
+
+                                    We specialise in a wide range of foods such as great quality pizzas, traditional COD and chips, kebabs, burgers and southern fried chicken.
+                                    We use the best ingredients to provide the best quality along with providing the best service. We specialise in traditional fish and chips and pizzas as these are our specialities and we have a combined experience of over 50 years. We endeavour to bring our customers the best quality food for the best price</p>
                                 <router-link :to="{ path: 'postal-code'}" class="anima-btn move-eff"><span>Order NOw</span></router-link>
                             </div>
                         </div>
