4957268f73e1328f388b80006e3dee79d6818768
yousufmunir1 <yousuf@bargoventures.com>
2020-06-28 00:47:15 +0500

    message

--- resources/js/components/order-page/popup/add-product.vue
+++ resources/js/components/order-page/popup/add-product.vue
@@ -10,8 +10,8 @@
                 <h4>Description</h4>
                 <p>{{list.description}}</p>
 
-                <h4>Allergy Information</h4>
-                <p>lorem ipsume lorem ipsume lorem ipsume lorem ipsume lorem ipsume lorem ipsume lorem ipsume lorem ipsume lorem ipsume lorem ipsume lorem ipsume lorem ipsume lorem ipsume lorem ipsume lorem ipsume lorem ipsume lorem ipsume lorem ipsume lorem ipsume lorem ipsume lorem ipsume lorem ipsume </p>
+                <h4 v-if="list.food_allergy">Allergy Information</h4>
+                <p v-if="list.food_allergy">{{list.food_allergy}} </p>
 
                 <form action="" method="">
 
