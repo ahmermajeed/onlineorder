384e362349cca4ba0c5953fc16a2fb1ef68705da
yousufmunir1 <yousuf@bargoventures.com>
2020-06-28 05:19:32 +0500

    updating

--- resources/js/components/common-components/nav-menu.vue
+++ resources/js/components/common-components/nav-menu.vue
@@ -3,7 +3,9 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
         <div class="container">
             <a class="navbar-brand" href="#">
+                <router-link :to="{ path: '/'}">
                 <img src="../../../images/mainlogo.png" alt="logo" style="    height: 40px;">
+                </router-link>
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
                     aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
