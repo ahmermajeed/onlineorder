4f1e4278ed473caa93c5c55914a754450d7b91a7
Areebsiddiqui <areebiqbal26@gmail.com>
2020-12-27 20:47:55 +0500

    hamza work

--- resources/js/components/common-components/footer.vue
+++ resources/js/components/common-components/footer.vue
@@ -7,14 +7,14 @@
                         <div class="col-md-3 footer-section-1">
                             <h3>Opening Hours</h3>
                             <ul class="left">
-                                <li><a href="#"><span><img src="../../../images/smartphone.png"></span>02476441200</a></li>
-                                <li><a href="#"><span><img src="../../../images/mail1.png"></span>info@mjstakeaway.co.uk.com</a></li>
-                                <li><a href="#"><span><img src="../../../images/location1.png"></span>10 Peckham Park Road, London  SE15 6TW </a></li>
+                                <li><a href="#"><span><img src="/images/smartphone.png"></span>02476441200</a></li>
+                                <li><a href="#"><span><img src="/images/mail1.png"></span>info@mjstakeaway.co.uk.com</a></li>
+                                <li><a href="#"><span><img src="/images/location1.png"></span>10 Peckham Park Road, London  SE15 6TW </a></li>
                             </ul>
                         </div>
                         <div class="col-md-6 footer-section-2">
                             <div class="text-center">
-                               <a href="#"><img src="../../../images/Ali-baba-logo.png"></a> 
+                               <a href="#"><img src="/images/Ali-baba-logo.png"></a> 
                             </div>
                             <div class="footer-menu">
                                 <ul>
@@ -30,11 +30,11 @@
                             </div>
                             <div class="icons text-center">
                                 <ul>
-                                    <li class="linked"><a href="#"><img src="../../../images/in.png"></a></li>
-                                    <li class="twitter"><a href="#"><img src="../../../images/in.png"></a></li>
-                                    <li class="pinterest"><a href="#"><img src="../../../images/in.png"></a></li>
-                                    <li class="google-plus"><a href="#"><img src="../../../images/in.png"></a></li>
-                                    <li class="wifi"><a href="#"><img src="../../../images/in.png"></a></li>
+                                    <li class="linked"><a href="#"><img src="/images/in.png"></a></li>
+                                    <li class="twitter"><a href="#"><img src="/images/in.png"></a></li>
+                                    <li class="pinterest"><a href="#"><img src="/images/in.png"></a></li>
+                                    <li class="google-plus"><a href="#"><img src="/images/in.png"></a></li>
+                                    <li class="wifi"><a href="#"><img src="/images/in.png"></a></li>
                                 </ul>
                                 
                             </div>
