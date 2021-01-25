42ea66c522131c1807c12f57eae443d04feef724
yousufmunir1 <yousuf@bargoventures.com>
2021-01-25 19:42:17 +0500

    updaitng

--- resources/js/components/common-components/footer.vue
+++ resources/js/components/common-components/footer.vue
@@ -8,14 +8,15 @@
                             <div class="col-lg-3 col-md-3 footer-section-1 footer-space order-1">
                                 <h3>Opening Hours</h3>
                                 <ul class="Opening">
-                                    <li><a href="#"><span><i class="icon-smartphone-1"></i></span>02476441200</a></li>
-                                    <li><a href="#"><span><i class="icon-mail-2"></i></span>info@mjstakeaway.co.uk.com</a></li>
-                                    <li><a href="#"><span><i class="icon-placeholder"></i></span>10 Peckham Park Road,London SE15 6TW</a></li>
+                                    <li><a href="#"><span><i class="icon-smartphone-1"></i></span>0203 490 2727</a></li>
+                                    <li><a href="#"><span><i class="icon-mail-2"></i></span>info@alibabakebab.co.uk</a></li>
+                                    <li><a href="#"><span><i class="icon-placeholder"></i></span>10 Peckham Park Road, London SE15 6TW
+                                    </a></li>
                                 </ul>
                             </div>
                             <div class="col-lg-6 col-md-6 footer-section-2 order-3 order-md-2">
                                 <div class="text-center">
-                                   <a href="#"><img src="/images/Ali-baba-logo.png"></a> 
+                                   <a href="#"><img src="/images/Ali-baba-logo.png"></a>
                                 </div>
                                 <div class="footer-menu text-center">
                                     <ul>
@@ -37,13 +38,14 @@
                                         <li class="google-plus"><a href="#"><i class="icon-google-plus-logo"></i></a></li>
                                         <li class="wifi"><a href="#"><i class="icon-rss-feed-symbol"></i></a></li>
                                     </ul>
-                                    
+
                                 </div>
                             </div>
                             <div class="col-lg-3 col-md-3 footer-section-3 footer-space order-2 order-md-3">
                                 <h3>Opening Hours</h3>
                                 <ul class="Opening-hours">
                                     <li>
+
                                         <strong><a href="#">Open 7 days a week</a></strong>
                                     </li>
                                     <li>
@@ -58,16 +60,16 @@
                                 </ul>
                             </div>
                         </div>
-                    </div>  
+                    </div>
                 </div>
-                
+
                 <div class="copyright text-center">
                     <div class="container">
                         <p>Copyright © 2020 | All rights reserved</p>
                     </div>
                 </div>
             </div>
-            
+
         </footer>
     </div>
 </template>
