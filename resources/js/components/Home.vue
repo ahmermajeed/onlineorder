a4a976559f42cda6a3e3f4bf8ba900ef3dcf1e40
SumayyaWaseem <sumayya.waseem@startrum.com>
2021-05-03 23:42:34 +0500

    food rating added

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -302,6 +302,41 @@
             </div>
             </div>
         </section>
+
+        <section>
+          <div class="food-agency-rating">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-12">
+                  <h2>Food Hygine Rating</h2>
+                          <div class="food-standard-bar">
+                              <img src="images/new-design/food/fsa-rating.png" alt="">
+                              <span class="title">FOOD HYGINE RATING</span>
+                              <span class="rating-count">0
+                                  <span class="rating-text">Urgent improvement necessary</span>
+                              </span>
+                              <span class="rating-count">1
+                                  <span class="rating-text">Major improvement necessary</span>
+                              </span>
+                              <span class="rating-count">2
+                                  <span class="rating-text">Improvment necessary</span>
+                              </span>
+                              <span class="rating-count">3
+                                  <span class="rating-text">Generally Satisfactory</span>
+                              </span>
+                              <span class="rating-count">4
+                                  <span class="rating-text">Good</span>
+                              </span>
+                              <span class="rating-count active">5
+                                  <span class="rating-text">Very Good</span>
+                              </span>
+                              <p class="note">This scheme is operated in partnership with your local authority</p>
+                          </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
  
 
         <section class="section-fullwidth reservation">
