b067a3150f0d4bccc6d0af13c53c468bc0fdeb29
SumayyaWaseem <sumayya.waseem@startrum.com>
2021-05-04 00:07:53 +0500

    food rating n delivery added

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -310,7 +310,7 @@
                 <div class="col-md-12">
                   <h2>Food Hygine Rating</h2>
                           <div class="food-standard-bar">
-                              <img src="images/new-design/food/fsa-rating.png" alt="">
+                              <img src="images/dixy-chicken/fsa-rating.png" alt="">
                               <span class="title">FOOD HYGINE RATING</span>
                               <span class="rating-count">0
                                   <span class="rating-text">Urgent improvement necessary</span>
@@ -331,6 +331,35 @@
                                   <span class="rating-text">Very Good</span>
                               </span>
                               <p class="note">This scheme is operated in partnership with your local authority</p>
+                          </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div class="food-delivery">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-12">
+                          <div class="deliver-areas" id="style-3">
+                      <h2>We are offering Deliveries in these areas</h2>
+                              <div class="postal-code">
+                                  <span>Cv1</span>
+                                  <span>Cv2</span>
+                                  <span>Cv3</span>
+                                  <span>Cv4</span>
+                                  <span>Cv5</span>
+                                  <span>Cv6</span>
+                                  <span>Cv7</span>
+                                  <span>Cv8</span>
+                                  <span>Cv9</span>
+                                  <span>Cv10</span>
+                                  <span>Cv11</span>
+                                  <span>Cv12</span>
+                                  <span>Cv13</span>
+                              </div>
                           </div>
                 </div>
               </div>
