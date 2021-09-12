bee134ab029c3e63a31c7cac12a9c1d91192f11e
DESKTOP-HEDNOLB\HP <osama@bargoventures.com>
2021-09-12 21:14:11 +0500

    Color & logo Changes

--- resources/js/components/common-components/nav-menu.vue
+++ resources/js/components/common-components/nav-menu.vue
@@ -38,7 +38,7 @@
                             <div class="col-6 col-md-3">
                                 <router-link :to="{ path: '/'}">
                                      <a class="navbar-brand js-scroll-trigger main-logo" href="javascript:;">
-                                        <img src="/images/tasty-land/logo.jpg">
+                                        <img src="/images/tasty-land/logo.png">
                                     </a>
                                 </router-link>
                             </div>
