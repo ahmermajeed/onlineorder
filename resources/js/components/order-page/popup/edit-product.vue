3c8be5a4d8a8da66c82083bcf1fc95a69447c422
DESKTOP-MOMI851\Owais Hameed <owais@bargoventures.com>
2021-09-26 18:23:01 +0500

    add and edit product

--- resources/js/components/order-page/popup/edit-product.vue
+++ resources/js/components/order-page/popup/edit-product.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <b-modal id="edit-product" centered @hidden="onHidden" :hide-footer=true title-tag="h4" ok-variant="primary" ref="myModalRef" no-close-on-backdrop modal-class="custom-modal order-product custom-btm-popup">
+        <b-modal id="edit-product" ok-only ok-title="Update to Order" @ok="addToCart()" centered @hidden="onHidden" title-tag="h4" ok-variant="primary" ref="myModalRef" no-close-on-backdrop modal-class="custom-modal order-product custom-btm-popup">
             <b-alert show variant="danger" v-if="error_message" style="text-transform: capitalize;">{{error_message}}</b-alert>
             <template #modal-title>{{list.name}}</template>
             <div class="product-gallery text-center" >
@@ -19,19 +19,36 @@
 
                 <form action="" method="">
 
-                    <div v-if="has_sizes">
+                    <div v-if="has_sizes" class="sub-cat addcart-form mb-3">
                         <h4>Sizes</h4>
                         <ul class="selectionlist radio-list" >
                             <li v-for="(item, index) in list.sizes" >
                                 <label>
-                                    <input type="radio" :value="item.size+'##@@'+priceFormat(item.price)"    v-model="productData['size']"  @click="productSum('size',item.price)">
-                                    {{item.size}}
+                                    <input type="radio" :value="item.size+'##@@'+priceFormat(item.price)" v-model="productData['size']"  @click="productSum('size',item.price, item)">
+                                      {{item.size}}
                                     
                                     <span class="checkmark"></span>
                                 </label>
-                                <span style="float:right"> £  {{item.price}}</span>
+                                <span style="float:right" v-if="item.price <= 0"> Free</span>
+                                <span style="float:right" v-else>£  {{item.price}}</span>
                             </li>
                         </ul>
+                    </div>
+
+                    <div v-if="sizeGroups.length > 0" class="sub-cat mt-3" v-for="(item, index) in sizeGroups">
+                      <h4>{{item.group.name}}</h4>
+                      <ul class="selectionlist radio-list" >
+                        <li v-for="(choice,choice_index) in item.group.choices" >
+                          <label>
+                            <input type="radio" :value="choice.name+'##@@'+priceFormat(choice.price)"   v-model="productData[item.group.name]" @click="productSum(item.name,choice.price, item)" >
+                            {{choice.name}}
+                            <span class="checkmark"></span>
+
+                          </label>
+                          <span style="float:right" v-if="choice.price <= 0"> Free</span>
+                          <span style="float:right" v-else>£  {{choice.price}}</span>
+                        </li>
+                      </ul>
                     </div>
 
 
@@ -40,15 +57,13 @@
                         <ul class="selectionlist radio-list" >
                             <li v-for="(choice,choice_index) in item.choices" >
                                 <label>
-                                     <input type="radio"  :value="choice.name+'##@@'+priceFormat(choice.price)"   v-model="productData[item.name]" @click="productSum(item.name,choice.price)" >
+                                     <input type="radio" :value="choice.name+'##@@'+priceFormat(choice.price)"   v-model="productData[item.name]" @click="productSum(item.name,choice.price,item)" >
                                     {{choice.name}}
-
-                                    {{item.name}}
-                                   
                                     <span class="checkmark"></span>
 
                                 </label>
-                                <span style="float:right"> £  {{choice.price}}</span>
+                                <span style="float:right" v-if="choice.price <= 0"> Free</span>
+                                <span style="float:right" v-else>£  {{choice.price}}</span>
                             </li>
                         </ul>
                     </div>
@@ -65,16 +80,17 @@
                                 </div> -->
                                 <div class="select-num text-right">
                                     <div class="qunt-btn">
-                                        <button type="button" class="btn-plus"  @click.prevent="plusQuantity()" >
-                                            <i class="icon-plus"></i>
-                                        </button>
-                                     
-                                        <span class="btn-badge-count">{{product_quantity}}</span> 
 
-                                        <button type="button" class="btn-minus" @click.prevent="minusQuantity()">
-                                       <i class="icon-subtract"></i>
-                                        </button> 
-                                      
+                                      <button type="button" class="btn-minus" @click.prevent="minusQuantity()">
+                                        <i class="icon-subtract"></i>
+                                      </button>
+
+                                        <span class="btn-badge-count">{{product_quantity}}</span>
+
+                                      <button type="button" class="btn-plus"  @click.prevent="plusQuantity()" >
+                                        <i class="icon-plus"></i>
+                                      </button>
+
                                     </div>
                                     
                                 </div>
@@ -89,9 +105,9 @@
                                
                         </div> -->
                     </div>
-                    <div class="modal-ftr">
+<!--                    <div class="modal-ftr">
                         <button  @click.prevent="addToCart()" class="custom-btn add-count-button btn btn-rounded-danger">Update <span class="text-right">{{priceFormat(total_amount_of_single_product * product_quantity)}}</span></button>
-                    </div>
+                    </div>-->
                 </form>
 
             </div>
@@ -112,7 +128,8 @@ export default {
       total_amount_of_single_product : 0,
       product_array:{},
       error_message:'',
-      totalPrice : 0
+      totalPrice : 0,
+      sizeGroups : []
     };
   },
   methods: {
@@ -133,11 +150,24 @@ export default {
       this.product_quantity = this.editList.quantity;
 
       this.totalPrice = this.editList.price * this.editList.quantity;
+      this.total_amount_of_single_product = this.editList.price
 
       for (var choice in this.editList.extras) {
         let index = this.editList.extras[choice]['group_name'];
+
+        if(index == 'size') {
+          for (var ss in this.list.sizes) {
+            for (var ed in this.editList.extras) {
+                if(this.editList.extras[ed].choice == this.list.sizes[ss].size) {
+                    this.sizeGroups = this.list.sizes[ss].size_groups;
+                }
+            }
+          }
+        }
+
         this.productData[index] = this.editList.extras[choice]['choice']+'##@@'+this.priceFormat(this.editList.extras[choice]['price']);
       }
+
     },
     onHidden() {
       this.$emit('HideModalValue');
@@ -151,6 +181,7 @@ export default {
       this.error_message = '';
       this.$refs.myModalRef.hide();
       this.$bvModal.hide('edit-product');
+      this.sizeGroups = [];
 
     },
     addToCart()
@@ -229,13 +260,13 @@ export default {
 
     plusQuantity(){
       this.product_quantity ++;
-      this.productSum()
+      //this.productSum()
     },
 
     minusQuantity(){
       if(this.product_quantity  > 1){
         this.product_quantity --;
-        this.productSum()
+       // this.productSum()
       }
     },
     checkMandatory(){
@@ -254,10 +285,43 @@ export default {
       return mandatory;
     },
 
-    productSum(){
+    productSum(array_key,value, item){
+
       setTimeout(()=>{
         let total = 0;
         let vm  = this;
+
+        if(item.size_groups) {
+          this.sizeGroups = item.size_groups
+
+          console.log(this.productData)
+
+          var keys = Object.keys(this.productData)
+          console.log(keys)
+
+          Object.keys(this.productData).forEach(function(itm){
+            if(itm != "size") delete vm.productData[itm];
+          });
+
+
+          /*if (this.productData['size'] != "size") {
+            console.log(this.productData)
+         }
+
+          this.productData.keys(this.productData).filter(key =>
+              key !== 'size').reduce((obj, key) =>
+              {
+
+                console.log(this.productData[key]);
+
+
+                obj[key] = this.productData[key];
+                return obj;
+              }, {}
+          );*/
+
+        }
+
         if(!this.has_sizes){
           total =  parseFloat(this.list.price);
         }
