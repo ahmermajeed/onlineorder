95e8eda311c8f11829d19da0f8fd7d18c219a9a8
DESKTOP-MOMI851\Owais Hameed <owais@bargoventures.com>
2021-09-29 00:13:40 +0500

    ll

--- resources/js/components/order-page/main.vue
+++ resources/js/components/order-page/main.vue
@@ -113,36 +113,7 @@
                                   </b-card-body>
                                 </b-collapse>
                               </b-card>
-
-                              <b-card no-body class="mb-1">
-                                <b-card-header header-tag="header" class="p-1" role="tab">
-                                  <b-button block v-b-toggle.accordion-2>Pizza
-                                    <span class="icon-up-arrow"></span>
-                                  <span class="icon-down-arrow"></span>
-                                  </b-button>
-
-                                </b-card-header>
-                                <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
-                                  <b-card-body>
-                                    <b-card-text>{{ text }}</b-card-text>
-                                  </b-card-body>
-                                </b-collapse>
-                              </b-card>
-
-                              <b-card no-body class="mb-1">
-                                <b-card-header header-tag="header" class="p-1" role="tab">
-                                  <b-button block v-b-toggle.accordion-3>GDK Signature Sauce Dips
-                                    <span class="icon-up-arrow"></span>
-                                    <span class="icon-down-arrow"></span>
-                                  </b-button>
-
-                                </b-card-header>
-                                <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
-                                  <b-card-body>
-                                    <b-card-text>{{ text }}</b-card-text>
-                                  </b-card-body>
-                                </b-collapse>
-                              </b-card>
+                            
                           </div>
                         </section>
                     </div>
