db1fdec830d2f99227aa33550f7191f818e0e5c1
DESKTOP-MOMI851\Owais Hameed <owais@bargoventures.com>
2021-10-05 02:02:28 +0500

    ll

--- app/Http/Controllers/Api/DeliveryChargesController.php
+++ app/Http/Controllers/Api/DeliveryChargesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Data\Models\RestaurantTiming;
+use App\Data\Models\RestaurantTimingSpecific;
 use App\Data\Repositories\DeliveryChargesRepository;
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
@@ -58,26 +60,78 @@ class DeliveryChargesController extends Controller
     {
         $returnArray = array();
 
-        for ($j = 0; $j <= 2; $j++) {
+        $show_asap = 0;
 
-            $currentDateTime = Carbon::now();
-            $currentDateTime->addDay($j);
-            $day = $currentDateTime->format('l');
+        $available_days = RestaurantTiming::where('shop_close', 1)->get(); // 1 matlb shop khuli ha
+
+        foreach ($available_days as $days) {
+
+            $dayDate = new Carbon('next ' . $days['day']);
+            $today = new Carbon('today');
+
+            $todayDay = $today->format('l');
+
+            $getSpecificDate = $dayDate->format('Y-m-d');
+
+            if($todayDay == $days['day']) {
+                $getSpecificDate = $today->format('Y-m-d');
+            }
+
+            $specific = RestaurantTimingSpecific::where('specific_date', $getSpecificDate)->first();
+
+            if($specific) {
+
+                $days['start_time'] = $specific['start_time'];
+                $days['end_time'] = $specific['end_time'];
+
+            }
 
             $time = array();// Define output
-            $StartTime = strtotime("12:30"); //Get Timestamp
-            $EndTime = strtotime("23:55"); //Get Timestamp
+
+            if($todayDay == $days['day']) {
+
+                $current_timestamp = Carbon::now()->timestamp;
+                $current_date_time = Carbon::now()->format('H:30:00');
+
+                $StartTime = strtotime($days['start_time']); //Get Timestamp
+
+                if($current_timestamp >= $StartTime) {
+
+                    $current_date_time = Carbon::parse($current_date_time)->addMinutes(30)->format("H:i:s");
+
+                    $StartTime = strtotime($current_date_time);
+                    $EndTime = strtotime($days['end_time']); //Get Timestamp
+                } else {
+                    $StartTime = strtotime($days['start_time']); //Get Timestamp
+                    $EndTime = strtotime($days['end_time']); //Get Timestamp
+                }
+
+            } else {
+                $StartTime = strtotime($days['start_time']); //Get Timestamp
+                $EndTime = strtotime($days['end_time']); //Get Timestamp
+            }
 
             $AddMins = $duration * 60;
 
             while ($StartTime <= $EndTime) //Run loop
             {
-                $time[] = $day. " ". date("G:i", $StartTime);
-                $StartTime += $AddMins; //Endtime check
+                if ($todayDay == $days['day']) {
+                    $show_asap = 1;
+                }
+
+                $time[] = $days['day']. " ".$getSpecificDate." at ". date("G:i", $StartTime);
+                $StartTime += $AddMins;  // Endtime check
             }
 
             array_push($returnArray, $time);
+
         }
+
+        if($show_asap == 1) {
+            array_push($returnArray, "As soon as possible");
+        }
+
+        asort($returnArray);
 
         $final_array = $this->_repository->array_flatten($returnArray);
 
